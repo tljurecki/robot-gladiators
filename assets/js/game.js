fccdf2ad3e7fb80922d17a66c541cb03489953e1
Tanya Jurecki <tljurecki@gmail.com>
2022-03-01 08:53:24 -0500

    added if else commands to stop the fight at a health of 0

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,40 @@
 var playerName = window.prompt("What is your robot's name?");
-//What is this?
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-//this will do math an log 20
-console.log(10+10);
-//what is this?
-console.log("Our robot's name is " + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight() {
-    window.alert("The fight has begun!");
-  }
-//fight();
+var fight = function () {
+    window.alert("Welcome to Robot Gladiators");
+    //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
+    //Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+    //check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName = "has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
+    playerHealth = playerHealth - enemyAttack;
+    //Log a resulting message to the console so we know that it worked.
+    console.log(
+    enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining."
+    );
+    //check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died! ");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left. ");
+    }
+
+    
+};
+fight();
