a44b62ec3d326d28059b4a0c7d727a83d7adae00
Tanya Jurecki <tljurecki@gmail.com>
2022-03-01 12:21:18 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,7 +14,7 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function (enemyName) {
-    while(playerHealth > 0 && enemyHealth > 0) {
+    while (playerHealth > 0 && enemyHealth > 0) {
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?  Enter 'FIGHT' or 'SKIP to choose. ");
     //if player choses to fight, then fight
    
@@ -62,7 +62,15 @@ var fight = function (enemyName) {
 };  
      
 for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators!  Round " + ( i + 1));
+    
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
-};
+}
+else {
+    window.alert('You have lost your robot in battle! Game Over!');
+    break;
+  }
+}
