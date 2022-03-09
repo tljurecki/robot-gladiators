86b54f41c145d7685efc1f1d52d528089e1c1da9
Tanya Jurecki <tljurecki@gmail.com>
2022-03-09 11:46:06 -0500

    randomize order in fight() function

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,7 +22,12 @@ var fightOrSkip = function() {
 
 // fight function (now with parameter for enemy's name)
 var fight = function(enemy) {
+ var isPlayerTurn = true;
+ if(Math.random () > 0.5) {
+   isPlayerTurn = false;
+ }
   while (playerInfo.health > 0 && enemy.health > 0) {
+    if(isPlayerTurn) {}
     // ask player if they'd like to fight or run
     if(fightOrSkip());{
       break;
@@ -46,6 +51,7 @@ var fight = function(enemy) {
     } else {
       window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
     }
+  } else {
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
@@ -63,6 +69,8 @@ var fight = function(enemy) {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
   }
+  isPlayerTurn = !isPlayerTurn; 
+}
 };
 
 
